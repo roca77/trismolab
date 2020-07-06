4a3cf6b623ce1067b1dab6d1f91af13c4dd9e734
roca77 <youssoufj@gmail.com>
2020-07-05 21:13:22 -0400

    Created user sign in page, login, and redirecting to a profile page.

--- trismotutoring/trismo/form.py
+++ trismotutoring/trismo/form.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
-from .models import Learner, Teacher, User
+from .models import Learner, Teacher, User, Profile
 
 
 class LearnerSignUpForm(UserCreationForm):
-    email = forms.CharField(required=True)
+    email_address = forms.CharField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
 
@@ -14,8 +14,9 @@ class LearnerSignUpForm(UserCreationForm):
 
     @transaction.atomic
     # This allows us to quickly save some data while the user is being created
-    def data_save(self):
+    def save(self):
         user = super().save(commit=False)
+        user.is_learner = True
         user.first_name = self.cleaned_data.get('first_name')
         user.last_name = self.cleaned_data.get('last_name')
         user.email = self.cleaned_data.get('email_address')
@@ -26,7 +27,7 @@ class LearnerSignUpForm(UserCreationForm):
 
 
 class TeacherSignUpForm(UserCreationForm):
-    email = forms.CharField(required=True)
+    email_address = forms.CharField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
 
@@ -34,8 +35,9 @@ class TeacherSignUpForm(UserCreationForm):
         model = User
 
     @transaction.atomic
-    def data_save(self):
+    def save(self):
         user = super().save(commit=False)
+        user.is_teacher = True
         user.first_name = self.cleaned_data.get('first_name')
         user.last_name = self.cleaned_data.get('last_name')
         user.email = self.cleaned_data.get('email_address')
@@ -43,3 +45,25 @@ class TeacherSignUpForm(UserCreationForm):
         teacher = Teacher.objects.create(user=user)
         teacher.save()
         return teacher
+
+
+class UserProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['first_name',
+                  'last_name',
+                  'email',
+                  'confirm_email',
+                  'date_of_birth',
+                  'bio',
+                  'avatar'
+                  ]
+
+    def clean(self):
+        """Checks if email1 and email2 are the same"""
+        cleaned_data = super().clean()
+        email = cleaned_data['email']
+        confirm = cleaned_data['confirm_email']
+        if email != confirm:
+            raise forms.ValidationError("you need to enter the same email in both fields")
--- trismotutoring/trismo/models.py
+++ trismotutoring/trismo/models.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.core import validators
+from django.contrib.auth.models import User
 
 
 class User(AbstractUser):
@@ -14,6 +17,7 @@ class Learner(models.Model):
     education_level = models.CharField(max_length=100)
     email_address = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20)
+    last_login = models.DateTimeField(default=timezone.now)
 
 
 class Teacher(models.Model):
@@ -21,6 +25,23 @@ class Teacher(models.Model):
     speciality = models.CharField(max_length=100)
     email_address = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=20)
+    last_login = models.DateTimeField(default=timezone.now)
+
+
+class Profile(models.Model):
+    # if the user is the deleted the profile will be deleted
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(default="")
+    confirm_email = models.EmailField(default="")
+    first_name = models.CharField(default="", max_length=250)
+    last_name = models.CharField(default="", max_length=250)
+    date_of_birth = models.DateField(null=True)
+    bio = models.TextField(validators=[validators.MinLengthValidator(10)])
+    avatar = models.ImageField(upload_to='profile_pics', default='default.jpg')
+
+    def __str__(self):
+        """Prints username from database as string"""
+        return f'{self.user.username} Profile'
 
 
 
